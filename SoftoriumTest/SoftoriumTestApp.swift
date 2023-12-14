0c11f272c30e6ab283a2ef47c69420563b44bd08
GimmyFox <mguzeev99@gmail.com>
2023-12-14 14:08:17 +0500

    Initial Commit

---
+++ SoftoriumTest/SoftoriumTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftoriumTestApp.swift
+//  SoftoriumTest
+//
+//  Created by Maksim Guzeev on 14.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SoftoriumTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
